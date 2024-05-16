0642efdc3a136a1b850d95d7e742229e4ca0e9ff
farahfauziahiqlima <116136911+farahfauziahiqlima@users.noreply.github.com>
2024-05-16 11:54:49 +0700

    main

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,11 @@ import os
 from pathlib import Path
 import shutil
 
-st.set_page_config(
-    page_title="InClouds",
-    page_icon="⛅",
+##st.set_page_config(
+  ##  page_title="InClouds",
+   ### page_icon="⛅",
 
-)
+##)
 
 # Konfigurasi direktori utama
 BASE_DIR = Path("uploads")
