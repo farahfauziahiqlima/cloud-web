a8b13718d2824dbae4b575f14c0b57d843c28d17
farahfauziahiqlima <116136911+farahfauziahiqlima@users.noreply.github.com>
2024-05-16 11:24:40 +0700

    main

--- streamlit_app.py
+++ streamlit_app.py
@@ -118,7 +118,7 @@ def tampilkan_isi_folder(path):
                         st.code(content, language=file.suffix.lstrip("."))
 
 # Aplikasi Streamlit
-st.title("Penyimpanan LDK Kahfi YARSI")
+st.title("Penyimpanan LDK Kahfi - YARSI")
 
 # Membaca query parameter untuk navigasi folder
 query_params = st.experimental_get_query_params()
